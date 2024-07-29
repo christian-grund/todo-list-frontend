28c99f62d7e3301a3fe073ca46fe2133d111344b
christian-grund <151537557+christian-grund@users.noreply.github.com>
2024-07-29 12:54:19 +0200

    02 - Angular Frontend anlegen

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss',
+})
+export class LoginComponent {
+  email: string = '';
+  password: string = '';
+
+  login() {
+    // Logik, um mit Backend zu kommunizieren
+  }
+}
